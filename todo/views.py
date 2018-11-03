155892a380e0aed7337d30884ee94fc671e01f59
michael Li <michael.lizhiyuan@gmail.com>
2018-11-03 14:41:01 -0400

    First commit

---
+++ todo/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import TodoItem
+# Create your views here.
+def todoView(request):
+    all_todo_items = TodoItem.objects.all()
+    return render(request,'todo.html',{'all_items':all_todo_items})
